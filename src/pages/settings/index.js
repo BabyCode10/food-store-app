28e730960ac23519efbca0b4bc4353307ea6ad3d
irfan hadian <irfan.hadian10@gmail.com>
2020-10-28 07:59:24 +0700

    updated settings index

--- src/pages/settings/index.js
+++ src/pages/settings/index.js
@@ -1,15 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Main from "../../layouts/main";
 import { Search } from "react-feather";
 
 const Settings = () => {
-  const settings = [
+  const [search, setSearch] = useState("");
+  const menus = [
     { label: "Menu", link: "menu" },
     { label: "Category", link: "category" },
     { label: "User", link: "user" },
   ];
 
+  const settings = [];
+  menus.forEach((menu, index) => {
+    if (menu.label.toUpperCase().indexOf(search.toUpperCase()) === -1) {
+      return;
+    }
+
+    settings.push(
+      <Link
+        key={index}
+        className="flex items-center text-sm text-gray-800 font-medium border-2 rounded-xl group hover:bg-indigo-700 hover:border-indigo-700 hover:text-white p-2"
+        to={`/settings/${menu.link}`}
+      >
+        <div className="w-8 h-8 bg-gray-400 rounded-xl mr-4 group-hover:bg-indigo-500"></div>
+        {menu.label}
+      </Link>
+    );
+  });
   return (
     <Main>
       <div className="flex-1 flex flex-col px-8">
@@ -17,26 +35,17 @@ const Settings = () => {
           <Search className="text-gray-300 mr-2" size={20} />
 
           <input
-            className="text-md text-gray-800 w-full bg-transparent focus:outline-none"
+            className="w-full text-md text-gray-800 bg-transparent focus:outline-none"
             type="text"
             placeholder="Search..."
+            onChange={(event) => setSearch(event.target.value)}
+            value={search}
           />
         </div>
 
         <h6 className="text-xl text-gray-800 font-bold mb-8">Settings</h6>
 
-        <div className="grid grid-cols-3 gap-4">
-          {settings.map((setting, index) => (
-            <Link
-              key={index}
-              className="flex items-center text-sm text-gray-800 font-medium border-2 rounded-xl group hover:bg-indigo-700 hover:border-indigo-700 hover:text-white p-2"
-              to={`/settings/${setting.link}`}
-            >
-              <div className="w-8 h-8 bg-gray-400 rounded-xl mr-4 group-hover:bg-indigo-500"></div>
-              {setting.label}
-            </Link>
-          ))}
-        </div>
+        <div className="grid grid-cols-3 gap-4">{settings}</div>
       </div>
     </Main>
   );
